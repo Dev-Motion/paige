4fe7b496cba814dcee952e606a22c85c9b2d8456
Your name <oluwaborihamzat@gmail.com>
2022-11-07 14:52:06 +0100

    added support for absolute path in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "stitches.config",
+        replacement: path.resolve(__dirname, "stitches.config.ts"),
+      },
+      {
+        find: "@utils",
+        replacement: path.resolve(__dirname, "src/utils"),
+      },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+    ],
+  },
+});
