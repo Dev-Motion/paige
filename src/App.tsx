a75e9da82ed2fe480cded85deed7195850426afb
Your name <oluwaborihamzat@gmail.com>
2022-11-08 17:09:29 +0100

    feature: created image-cache and initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Box, Text } from "@components/base";
+import { Main } from "@components/inc";
+import ImageProvider, { useImage } from "@utils/ImageContext";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <ImageProvider>
+      <Main />
+      {/* <Test /> */}
+      <ImageCache />
+    </ImageProvider>
+  );
 }
 
-export default App
+const ImageCache = () => {
+  const image = useImage();
+  return (
+    <Box css={{ display: "none" }}>
+      {image?.map((img, index) => {
+        return (
+          <img
+            key={index}
+            src={img.raw + "w=1920q=80&auto=format"}
+            alt="image"
+          />
+        );
+      })}
+    </Box>
+  );
+};
+export default App;
