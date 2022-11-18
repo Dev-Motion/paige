d49489c7d435458d6485034cc9c006144c00ab56
Your name <oluwaborihamzat@gmail.com>
2022-11-18 16:13:02 +0100

    refactor: moved imageContext to a context folder

--- src/context/ImageContext.tsx
+++ src/context/ImageContext.tsx
@@ -1,9 +1,9 @@
 import getImage from "@utils/getImage";
 import React, { createContext, useContext, useEffect } from "react";
 import { useLocalStorage } from "react-use";
-import { formatDate } from ".";
+import { formatDate } from "@utils/index";
 
-
+const ERROR_MESSAGE = "You need internet connection to get updated images";
 export type ImageResponse = {
   for: string;
   blur_hash: string;
@@ -62,12 +62,24 @@ function getNewImages(
   const tomorrow = new Date(today);
   tomorrow.setDate(tomorrow.getDate() + 1);
   if (!renew) {
-    getImage({ count: 2, query }).then((response) => {
-      const newImages = response.result.map((image, i) => {
-        if (i == 1) {
+    getImage({ count: 2, query })
+      .then((response) => {
+        const newImages = response.result.map((image, i) => {
+          if (i == 1) {
+            return {
+              for: formatDate(tomorrow),
+              img_url: image.img_url,
+              user_name: image.user_name,
+              user_link: image.user_link,
+              description: image.description,
+              blur_hash: image.blur_hash,
+              raw: image.raw,
+              color: image.color,
+            };
+          }
           return {
-            for: formatDate(tomorrow),
-            img_url: image.img_url,
+            for: formatDate(today),
+            img_url: image.raw,
             user_name: image.user_name,
             user_link: image.user_link,
             description: image.description,
@@ -75,41 +87,38 @@ function getNewImages(
             raw: image.raw,
             color: image.color,
           };
-        }
-        return {
-          for: formatDate(today),
-          img_url: image.raw,
-          user_name: image.user_name,
-          user_link: image.user_link,
-          description: image.description,
-          blur_hash: image.blur_hash,
-          raw: image.raw,
-          color: image.color,
-        };
+        });
+        setImageState(newImages);
+      })
+      .catch((e) => {
+        console.log(ERROR_MESSAGE);
+        setImageState((old) => [
+          old![1],
+          { ...old![1], for: formatDate(today) },
+        ]);
       });
-      setImageState(newImages);
-    });
   } else {
-    getImage({ count: 1, query }).then(({ result }) => {
-      const image = result[0];
-      setImageState((old) => [
-        old?.[1] as ImageResponse,
-        {
-          for: formatDate(tomorrow),
-          img_url: image.raw,
-          user_name: image.user_name,
-          user_link: image.user_link,
-          description: image.description,
-          blur_hash: image.blur_hash,
-          raw: image.raw,
-          color: image.color,
-        },
-      ]);
-      return;
-    });
+    getImage({ count: 1, query })
+      .then(({ result }) => {
+        const image = result[0];
+        setImageState((old) => [
+          old?.[1] as ImageResponse,
+          {
+            for: formatDate(tomorrow),
+            img_url: image.raw,
+            user_name: image.user_name,
+            user_link: image.user_link,
+            description: image.description,
+            blur_hash: image.blur_hash,
+            raw: image.raw,
+            color: image.color,
+          },
+        ]);
+      })
+      .catch((e) => {
+        console.log(ERROR_MESSAGE);
+      });
   }
 }
-
-
 
 export default ImageProvider;
