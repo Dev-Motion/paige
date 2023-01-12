0530372b03be5dcfbfe97a0b41ddb36d8b587c1b
Your name <oluwaborihamzat@gmail.com>
2023-01-12 13:31:26 +0100

    fix: cleaned up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,13 @@
-import React, { useEffect } from "react";
-import useStore from "@store";
-import { Box } from "@components/base";
-import { Main } from "@components/inc";
+import React from "react";
+import { MainLayout, Scaffold } from "@components/inc";
 
 function App() {
-  const setTheme = useStore((state) => state.setTheme);
-  useEffect(() => {
-    setTheme();
-  }, []);
   return (
     <>
-      <Main />
-      <ImageCache />
+      <MainLayout />
+      <Scaffold />
     </>
   );
 }
 
-const ImageCache = () => {
-  const [photos, setPhotos] = useStore((state) => [
-    state.photos,
-    state.setPhotos,
-  ]);
-  useEffect(() => {
-    const today = new Date().toDateString();
-    const todayImage = photos.filter(
-      (photo) => new Date(photo.for).toDateString() === today
-    )[0];
-    if (photos.length === 0 || !todayImage) {
-      setPhotos("mountains", false);
-    }
-  }, []);
-  return (
-    <Box css={{ display: "none" }}>
-      {photos?.map((photo, index) => {
-        return (
-          <img
-            key={index}
-            src={photo.urls.raw + "&w=2048&q=80&auto=format"}
-            alt="image"
-          />
-        );
-      })}
-    </Box>
-  );
-};
 export default App;
