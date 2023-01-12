668d3c6a4a1d28602fbd7c575fe00a0b35857d6d
Your name <oluwaborihamzat@gmail.com>
2023-01-12 13:22:43 +0100

    feat: added more constants

---
+++ src/constants/galleryTabs.ts
@@ -0,0 +1,23 @@
+const galleryTabs = [
+  {
+    value: "cloud",
+    name: "Cloud Wallpapers",
+  },
+  {
+    value: "favourites",
+    name: "Favourites",
+  },
+  {
+    value: "recent",
+    name: "Recently Used",
+  },
+  {
+    value: "default",
+    name: "Default",
+  },
+  {
+    value: "add",
+    name: "Add Photo",
+  },
+] as const;
+export default galleryTabs;
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export { default as galleryTabs } from "./galleryTabs";
+const imageQuality = "&w=2048&q=80&auto=format";
+export { imageQuality };
