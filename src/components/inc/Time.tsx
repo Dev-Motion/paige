42933f1ed17d0f8de95d45fa81cc7d0ef2f43d48
Your name <oluwaborihamzat@gmail.com>
2023-01-12 13:14:13 +0100

    feat: supports 24 hour clock

--- src/components/inc/Time.tsx
+++ src/components/inc/Time.tsx
@@ -1,34 +1,38 @@
 import React from "react";
-import { Box, Text } from "@components/base";
+import { Box, Flex, IconButton, Popover, Switch, Text } from "@components/base";
 import { More } from "@components/icons";
+import { accessibilityShadow } from "@components/base/utilityClass";
+import { getDaySegment, processTime } from "@utils";
+import { styled } from "stitches.config";
 
-const getDaySegment = (time: Date | null) => {
-  const hours = time?.getHours() || 0;
-  if (hours < 12 && hours >= 5) {
-    return "morning";
-  } else if (hours >= 12 && hours < 17) {
-    return "afternoon";
-  } else if (hours >= 17 && hours < 21) {
-    return "evening";
-  } else {
-    return "night";
-  }
+interface State {
+  time: Date;
+  visible: boolean;
+}
+function reducer(state: State, action: Partial<State>): State {
+  const newState = { ...state, ...action };
+  return newState;
+}
+const initialState = {
+  time: new Date(),
+  visible: false,
 };
+
 const Time = () => {
-  const [time, setTime] = React.useState(new Date());
-  const Hours = time.getHours();
-  const Minutes = time.getMinutes();
-  const isAM = Hours < 12;
-  const timeString = `${Hours.toString().padStart(
-    2,
-    "0"
-  )}:${Minutes.toString().padStart(2, "0")}`;
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  const [is24Hour, setIs24Hour] = React.useState(false);
+  const { timeString, isAM } = processTime(state.time, is24Hour);
   React.useEffect(() => {
     const interval = setInterval(() => {
-      setTime(new Date());
+      dispatch({ time: new Date() });
     }, 1000);
     return () => clearInterval(interval);
   }, []);
+
+  const openChange = (visible: boolean) => {
+    dispatch({ visible });
+  };
+  const onChecked = (checked: boolean) => setIs24Hour(checked);
   return (
     <Box
       css={{
@@ -36,41 +40,74 @@ const Time = () => {
         color: "$text",
         transition: "opacity 0.3s ease-in-out",
       }}
+      className={accessibilityShadow({ css: { $$blur: "40px" } })}
     >
       <Box
+        onMouseOver={() => dispatch({ visible: true })}
         css={{
           position: "relative",
-          "&:hover": {
-            svg: {
-              opacity: 1,
-            },
-          },
         }}
       >
         <Text as="h1" fs="6xl" fw={"bold"}>
           {timeString}
         </Text>
-        <More
-          css={{
-            position: "absolute",
-            right: -40,
-            opacity: 0,
-            transition: "all 0.3s ease-in-out",
-            top: "50%",
-            transform: "translateY(-50%)",
-          }}
-        />
+        <Popover
+          openChange={openChange}
+          content={<Menu checked={is24Hour} onChecked={onChecked} />}
+        >
+          <MoreButton size="sm" bg="transparent" visible={state.visible}>
+            <More css={{ circle: { fill: "$text !important" } }} />
+          </MoreButton>
+        </Popover>
       </Box>
-      <Text
-        fs="lg"
-        css={{ ml: "auto", display: "block", width: "min-content", mt: -20 }}
-      >
-        {isAM ? "am" : "pm"}
-      </Text>
+      {!is24Hour && (
+        <Text
+          fs="lg"
+          css={{ ml: "auto", display: "block", width: "min-content", mt: -20 }}
+        >
+          {isAM ? "am" : "pm"}
+        </Text>
+      )}
       <Text fs="2xl" fw="bold" ta="center" css={{ mt: "$2" }}>
-        Good {getDaySegment(time)} Victor
+        Good {getDaySegment(state.time)} Victor
       </Text>
     </Box>
+  );
+};
+
+const MoreButton = styled(IconButton, {
+  position: "absolute",
+  right: -40,
+  transition: "all 0.3s ease-in-out",
+  top: "50%",
+  color: "$text",
+  transform: "translateY(-50%)",
+  opacity: 0,
+  pointerEvents: "none",
+  "&:focus": {
+    opacity: 1,
+  },
+  variants: {
+    visible: {
+      true: {
+        opacity: 100,
+        pointerEvents: "all",
+      },
+    },
+  },
+});
+const Menu = ({
+  checked,
+  onChecked,
+}: {
+  checked: boolean;
+  onChecked: (check: boolean) => void;
+}) => {
+  return (
+    <Flex jc="between">
+      <Text as="label">24-hour clock</Text>
+      <Switch checked={checked} onCheckedChange={onChecked} />
+    </Flex>
   );
 };
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,26 @@ export function formatDate(date: Date) {
     day: "2-digit",
   });
 }
+export function getDaySegment(time: Date | null) {
+  const hours = time?.getHours() || 0;
+  if (hours < 12 && hours >= 5) {
+    return "morning";
+  } else if (hours >= 12 && hours < 17) {
+    return "afternoon";
+  } else if (hours >= 17 && hours < 21) {
+    return "evening";
+  } else {
+    return "night";
+  }
+}
+export function processTime(time: Date, is24Hour: boolean) {
+  const tHour = time.getHours();
+  const Hours = is24Hour ? tHour : tHour % 12;
+  const Minutes = time.getMinutes();
+  const isAM = tHour < 12;
+  const timeString = `${Hours.toString().padStart(
+    2,
+    "0"
+  )}:${Minutes.toString().padStart(2, "0")}`;
+  return { timeString, isAM };
+}
