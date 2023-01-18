1867c321d96a8c3f003c4e52db3262c0645e4ff4
Your name <oluwaborihamzat@gmail.com>
2023-01-18 21:18:28 +0100

    feat: moved to `index.ts` in utils

--- src/utils/preloadImages.ts
+++
@@ -1,9 +0,0 @@
-export default function preloadImages(images: string[]) {
-  images.forEach((image) => {
-    const link = document.createElement("link");
-    link.rel = "preload";
-    link.href = image;
-    link.as = "image";
-    document.head.appendChild(link);
-  });
-}
