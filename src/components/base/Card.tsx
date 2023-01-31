9d85d2c41e5cc581015429e0989997a514ab95a5
Your name <oluwaborihamzat@gmail.com>
2023-01-31 12:51:30 +0100

    feat: increased accessibility

--- src/components/base/Card.tsx
+++ src/components/base/Card.tsx
@@ -1,13 +1,12 @@
 import { styled } from "stitches.config";
-import { Box } from "@components/base";
+import Box from "./Box";
 
 const Card = styled(Box, {
   backdropFilter: "blur(40px)",
   br: "$4",
-  bg: "rgba($bgRGB, 0.2)",
+  bg: "rgba($bgRGB, 0.4)",
   color: "$text",
   minWidth: 420,
-  minHeight: 200,
 });
 
 export default Card;
