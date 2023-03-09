58ada61d869365310e4c7b3bf70889c21a070e1d
Your name <oluwaborihamzat@gmail.com>
2023-03-09 18:22:19 +0100

    feat: implement minimal code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,62 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+// split the code into multiple files based on REact.lazy components
+// https://vitejs.dev/guide/features.html#glob-import
 
-// https://vitejs.dev/config/
+function pathResolve(dir: string) {
+  return path.join(__dirname, ".", dir);
+}
+
+// https://vitejs.  /config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   resolve: {
     alias: [
       {
         find: "stitches.config",
-        replacement: path.resolve(__dirname, "stitches.config.ts"),
+        replacement: pathResolve("stitches.config.ts"),
       },
       {
         find: "@utils",
-        replacement: path.resolve(__dirname, "src/utils"),
+        replacement: pathResolve("src/utils"),
       },
       {
         find: "@context",
-        replacement: path.resolve(__dirname, "src/context"),
+        replacement: pathResolve("src/context"),
       },
       {
         find: "@components",
-        replacement: path.resolve(__dirname, "src/components"),
+        replacement: pathResolve("src/components"),
       },
       {
         find: "@store",
-        replacement: path.resolve(__dirname, "src/store"),
+        replacement: pathResolve("src/store"),
       },
       {
         find: "@constants",
-        replacement: path.resolve(__dirname, "src/constants"),
+        replacement: pathResolve("src/constants"),
       },
     ],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        // the default entry point
+        index: "./index.html",
+
+        // 1️⃣
+        "service-worker": "./src/service-worker.ts",
+      },
+
+      output: {
+        chunkFileNames: "assets/[name].min.js",
+        entryFileNames: (assetInfo) => {
+          return assetInfo.name === "service-worker"
+            ? "service-worker.js"
+            : "assets/[name].min.js";
+        },
+      },
+    },
   },
 });
