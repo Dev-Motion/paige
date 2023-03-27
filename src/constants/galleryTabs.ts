aae16e7a690c60e968785f4db3f0e78af5931db2
Your name <oluwaborihamzat@gmail.com>
2023-03-27 15:08:25 +0100

    feat: reduce gallery tabs

--- src/constants/galleryTabs.ts
+++ src/constants/galleryTabs.ts
@@ -7,17 +7,5 @@ const galleryTabs = [
     value: "favourites",
     name: "Favourites",
   },
-  {
-    value: "recent",
-    name: "Recently Used",
-  },
-  {
-    value: "default",
-    name: "Default",
-  },
-  {
-    value: "add",
-    name: "Add Photo",
-  },
 ] as const;
 export default galleryTabs;
