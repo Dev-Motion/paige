e22666119ec4800fd026bf3dd8a863811425017d
Your name <oluwaborihamzat@gmail.com>
2023-05-15 17:31:37 +0100

    feat: create build and dev options

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { dependencies } from "./package.json";
@@ -11,16 +12,72 @@ function renderChunks(deps: Record<string, string>) {
   });
   return chunks;
 }
-// split the code into multiple files based on REact.lazy components
-// https://vitejs.dev/guide/features.html#glob-import
 
 function pathResolve(dir: string) {
   return path.join(__dirname, ".", dir);
 }
 
-// https://vitejs.  /config/
+const buildTarget = process.env.BUILD_TARGET || "web";
+const isProduction = process.env.NODE_ENV === "production";
+const isWeb = buildTarget === "web";
+const isChrome = buildTarget === "chrome";
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Paige: your pesonal space",
+    short_name: "Paige",
+    description:
+      "a space where you can customize your focus, immerse yourself in calm scenes, and find inspiration whenever you open a new tab in your browser",
+    icons: [
+      {
+        src: "/logo/128x128.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/logo/128x128.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/logo/128x128.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/logo/128x128.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "landscape",
+  },
+  devOptions: {
+    enabled: true,
+  },
+  workbox: {
+    globPatterns: ["**/*.{js,css,html,ico,png,svg,tff}"],
+  },
+};
+
+const plugins = isWeb ? [react(), VitePWA(manifestForPlugin)] : [react()];
+const manualChunks =
+  isChrome && !isProduction
+    ? null
+    : {
+      vendor: ["react", "react-dom"],
+      ...renderChunks(dependencies),
+    };
 export default defineConfig({
-  plugins: [react()],
+  plugins,
   resolve: {
     alias: [
       {
@@ -74,10 +131,7 @@ export default defineConfig({
             ? "service-worker.js"
             : "assets/[name].min.js";
         },
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-          ...renderChunks(dependencies),
-        },
+        manualChunks,
       },
     },
   },
