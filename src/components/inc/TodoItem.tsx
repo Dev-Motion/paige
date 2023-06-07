e14246d9200f41e725ca859b93ef0f73f024184d
Your name <oluwaborihamzat@gmail.com>
2023-06-07 22:55:49 +0100

    feat: add support for date updates

--- src/components/inc/TodoItem.tsx
+++ src/components/inc/TodoItem.tsx
@@ -4,6 +4,7 @@ import {
   Dropdown,
   Flex,
   IconButton,
+  Popover,
   Text,
 } from "@components/base";
 import {
@@ -13,39 +14,81 @@ import {
   More,
   StarIcon,
 } from "@components/icons";
-import { motion } from "framer-motion";
 import useStore from "@store";
 import { Todo } from "@store/slices/todoSlice";
+import { analyzeDate } from "@utils";
+import { motion } from "framer-motion";
 import * as React from "react";
 import { styled } from "stitches.config";
-import { analyzeDate } from "@utils";
 import { shallow } from "zustand/shallow";
+import DatePicker from "./DatePicker";
+
+interface todoItemState {
+  text: string;
+  isEditing: boolean;
+  important: boolean;
+  dateTime?: Date;
+}
+function todoItemReducer(
+  state: todoItemState,
+  action:
+    | Partial<todoItemState>
+    | ((state: todoItemState) => Partial<todoItemState>)
+): todoItemState {
+  if (typeof action === "function") {
+    return { ...state, ...action(state) };
+  }
+  return { ...state, ...action };
+}
 
 function TodoItem({ todo }: { todo: Todo }) {
-  const [toggleTodo, editTodo, toggleImportant] = useStore(
-    (state) => [state.toggleTodo, state.editTodo, state.toggleImportant],
+  const [toggleTodo, editTodo, toggleImportant, setTodoDate] = useStore(
+    (state) => [
+      state.toggleTodo,
+      state.editTodo,
+      state.toggleImportant,
+      state.setTodoDate,
+    ],
     shallow
   );
-  const [text, setText] = React.useState(todo.text);
-  const [isEditing, setIsEditing] = React.useState(false);
-  const [show, setShow] = React.useState(false);
+  const [{ text, isEditing, important, dateTime }, setState] = React.useReducer(
+    todoItemReducer,
+    {
+      text: todo.text,
+      isEditing: false,
+      important: todo.important,
+      dateTime: todo.reminder ? new Date(todo.date) : undefined,
+    }
+  );
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
 
   const toggleEditing = () => {
-    setIsEditing((prev) => !prev);
+    setState((prev) => ({ isEditing: !prev.isEditing }));
   };
   const onEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter" && text) {
       editTodo(todo.id, text);
+      if (dateTime) {
+        setTodoDate(todo.id, dateTime);
+      }
+      if (todo.important !== important) {
+        toggleImportant(todo.id);
+      }
       toggleEditing();
-      setText("");
     }
   };
+
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isEditing]);
   return (
     <Card
       as={motion.div}
       layoutId={todo.id.toString()}
       // key={todo.id}
-      data-option-open={show}
       css={{
         display: "flex",
         gap: "$2",
@@ -67,93 +110,131 @@ function TodoItem({ todo }: { todo: Todo }) {
         overflow: "hidden",
       }}
     >
-      <CheckBox
-        checked={todo.completed}
-        onCheckedChange={() => toggleTodo(todo.id)}
-      />
-      {isEditing ? (
-        <Input
-          value={text}
-          onChange={(e) => setText(e.currentTarget.value)}
-          onKeyDown={onEnter}
-          ref={(e) => e?.focus()}
-        />
-      ) : (
-        <Flex fd="column" gap="1" css={{ flex: 1 }}>
-          <Text
-            fs="sm"
-            css={{
-              textDecoration: todo.completed ? "line-through" : "none",
-            }}
-          >
-            {todo.text}
-          </Text>
-          {todo.reminder && (
-            <Flex
+      {!isEditing ? (
+        <>
+          <CheckBox
+            checked={todo.completed}
+            onCheckedChange={() => toggleTodo(todo.id)}
+          />
+          <Flex fd="column" gap="1" css={{ flex: 1 }}>
+            <Text
+              fs="sm"
               css={{
-                border: "0.3px solid $text",
-                px: "$2",
-                py: "$1",
-                br: "$pill",
-                gap: "$1",
-                fontSize: "$2xs",
-                width: "fit-content",
-                color: "$text",
+                textDecoration: todo.completed ? "line-through" : "none",
               }}
             >
-              {analyzeDate(new Date(todo.date))}
-            </Flex>
-          )}
-        </Flex>
-      )}
-      <Flex
-        ai="center"
-        className="options"
-        css={{
-          position: "absolute",
-          overflow: "hidden",
-          top: "min(50%,23px)",
-          right: 0,
-          transform: "translateY(-50%) translateX(50%)",
-          transition: "all .3s ease-in-out",
-        }}
-      >
-        <IconButton
-          size="sm"
-          bg="transparent"
-          onClick={() => toggleImportant(todo.id)}
-        >
-          <StarIcon
+              {todo.text}
+            </Text>
+            {todo.reminder && (
+              <Flex
+                css={{
+                  border: "0.3px solid $text",
+                  px: "$2",
+                  py: "$1",
+                  br: "$pill",
+                  gap: "$1",
+                  fontSize: "$2xs",
+                  width: "fit-content",
+                  color: "$text",
+                }}
+              >
+                {analyzeDate(new Date(todo.date))}
+              </Flex>
+            )}
+          </Flex>
+          <Flex
+            ai="center"
+            className="options"
             css={{
-              size: "$4",
-              fill: todo.important ? "$text" : "none",
-              color: "$text",
+              position: "absolute",
+              overflow: "hidden",
+              top: "min(50%,23px)",
+              right: 0,
+              transform: "translateY(-50%) translateX(50%)",
+              transition: "all .3s ease-in-out",
             }}
-          />
-        </IconButton>
-        <Dropdown open={show} onOpenChange={setShow} key={todo.id}>
-          <Dropdown.Button asChild>
+          >
             <IconButton
               size="sm"
               bg="transparent"
-              onClick={() => setShow(true)}
-              css={{}}
+              onClick={() => toggleImportant(todo.id)}
             >
-              <More css={{ size: "$3", color: "$text" }} />
-              <Text css={{ include: "screenReaderOnly" }}>more options</Text>
+              <StarIcon
+                css={{
+                  size: "$4",
+                  fill: todo.important ? "$text" : "none",
+                  color: "$text",
+                }}
+              />
             </IconButton>
-          </Dropdown.Button>
-          <Dropdown.Menu>
-            <TodoItemOptions
-              id={todo.id}
-              toggleEdit={() => {
-                toggleEditing();
-                setShow(false);
-              }}
-            />
-          </Dropdown.Menu>
-        </Dropdown>
-      </Flex>
+            <Dropdown key={todo.id}>
+              <Dropdown.Button asChild>
+                <IconButton size="sm" bg="transparent" css={{}}>
+                  <More css={{ size: "$3", color: "$text" }} />
+                  <Text css={{ include: "screenReaderOnly" }}>
+                    more options
+                  </Text>
+                </IconButton>
+              </Dropdown.Button>
+              <Dropdown.Menu>
+                <TodoItemOptions
+                  id={todo.id}
+                  toggleEdit={() => {
+                    toggleEditing();
+                  }}
+                />
+              </Dropdown.Menu>
+            </Dropdown>
+          </Flex>
+        </>
+      ) : (
+        <Flex css={{ width: "100%" }}>
+          <Input
+            value={text}
+            onChange={(e) => setState({ text: e.currentTarget.value })}
+            onKeyDown={onEnter}
+            ref={inputRef}
+            css={{ flex: 1, pl: "$2" }}
+          />
+          <Flex>
+            <Popover>
+              <Popover.Button asChild>
+                <IconButton bg="transparent" size="xs">
+                  <AlarmIcon css={{ size: "$3", color: "$text" }} />
+                </IconButton>
+              </Popover.Button>
+              <Popover.Content>
+                <DatePicker
+                  date={dateTime}
+                  onChange={(dateTime) => {
+                    setState({ dateTime });
+                    inputRef.current?.focus();
+                  }}
+                />
+                <Popover.Close />
+              </Popover.Content>
+            </Popover>
+            <IconButton
+              bg="transparent"
+              size="xs"
+              onClick={() =>
+                setState((state) => ({
+                  important: !state.important,
+                }))
+              }
+            >
+              <StarIcon
+                css={{
+                  size: "$3",
+                  fill: important ? "$text" : "transparent",
+                  transition: "all 300ms ease-in-out",
+                  color: "$text",
+                }}
+              />
+            </IconButton>
+          </Flex>
+        </Flex>
+      )}
     </Card>
   );
 }
@@ -175,11 +256,6 @@ function TodoItemOptions({
       <Dropdown.MenuItem onClick={() => removeTodo(id)}>
         <Flex gap="1" ai="center">
           <DeleteIcon css={{ size: "$3" }} /> <Text fs="sm">Delete</Text>
-        </Flex>
-      </Dropdown.MenuItem>
-      <Dropdown.MenuItem>
-        <Flex gap="1" ai="center">
-          <AlarmIcon css={{ size: "$3" }} /> <Text fs="sm">Set reminder</Text>
         </Flex>
       </Dropdown.MenuItem>
     </>
