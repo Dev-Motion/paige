8e6ab8095a59cb02ad9d686fb588979df4cf3fed
Your name <oluwaborihamzat@gmail.com>
2023-06-24 18:32:03 +0100

    feat: insert `ErrorBoundary`

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Onboard from "./Onboard";
 import useStore from "@store";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorUI from "@components/inc/ErrorUI";
 
 function Router() {
   const onboarded = useStore((state) => state.name);
@@ -11,6 +13,8 @@ function Router() {
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <ErrorBoundary fallbackRender={ErrorUI}>
+      <Router />
+    </ErrorBoundary>
   </React.StrictMode>
 );
