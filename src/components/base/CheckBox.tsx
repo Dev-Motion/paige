66a3387fe43eb723f4f45c8dd60b5d27333d2328
Your name <oluwaborihamzat@gmail.com>
2023-07-14 18:31:06 +0100

    feat: update checkbox UI

--- src/components/base/CheckBox.tsx
+++ src/components/base/CheckBox.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentProps } from "react";
+import React, { ComponentProps, useEffect } from "react";
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { styled } from "stitches.config";
 import { AnimatePresence, motion } from "framer-motion";
@@ -14,8 +14,7 @@ export const CheckIcon = (props: ComponentProps<"svg">) => {
     >
       <motion.path
         initial={{ pathLength: 0 }}
-        animate={{ pathLength: 1, transition: { duration: 1 } }}
-        exit={{ pathLength: 0, transition: { duration: 1 } }}
+        animate={{ pathLength: 1, transition: { duration: 0.5 } }}
         transition={{
           delay: 0.2,
           type: "tween",
@@ -28,14 +27,24 @@ export const CheckIcon = (props: ComponentProps<"svg">) => {
     </svg>
   );
 };
-
+let rerender = 0;
 function CheckBox(props: Checkbox.CheckboxProps) {
   const checked = props.checked === "indeterminate" ? false : props.checked;
+  const formerChecked = !checked;
+  const [key, setKey] = React.useState("init");
+  // helps to prevenet the animation from playing on first render
+  useEffect(() => {
+    if (formerChecked === false) {
+      setKey("init-" + rerender);
+    }
+  }, [formerChecked]);
+  rerender++;
+
   return (
     <CheckboxRoot {...props} active={checked}>
       <CheckboxIndicator active={checked} forceMount>
         <AnimatePresence mode="wait">
-          {props.checked && <CheckIcon />}
+          <CheckIcon key={key} />
         </AnimatePresence>
       </CheckboxIndicator>
     </CheckboxRoot>
@@ -54,11 +63,16 @@ const CheckboxRoot = styled(Checkbox.Root, {
   alignItems: "center",
   justifyContent: "center",
   boxShadow: "0 2px 10px $colors$bg , 0 0 0 1px $colors$text inset",
-  "&:hover": {
-    backgroundColor: "rgba($textRGB,0.4)",
-    "& svg": { color: "$bg" },
+  "&:focus-visible": { boxShadow: "0 0 0 2px $colors$text inset" },
+  "&[data-state='checked'] svg": {
+    opacity: "1",
   },
-  "&:focus": { boxShadow: "0 0 0 2px $colors$text inset" },
+  "&[data-state='unchecked'] svg": {
+    opacity: 0,
+  },
+  "&:hover": {
+    "& svg": { opacity: 1, color: "rgba($textRGB,0.7)" },
+  },
   variants: {
     active: {
       true: {
@@ -72,6 +86,8 @@ const CheckboxRoot = styled(Checkbox.Root, {
 const CheckboxIndicator = styled(Checkbox.Indicator, {
   color: "$text",
   width: "60%",
+  transition: "all 0.2s ease",
+
   variants: {
     active: {
       true: {
