46d59630d6dbcaec88380a2a7e48377ce2951519
Hamzat Victor <oluwaborihamzat@gmail.com>
2023-12-11 23:53:07 +0100

    feat: add chrome semantic release plugin

--- release.config.cjs
+++ release.config.cjs
@@ -4,12 +4,18 @@
 module.exports = {
   branches: ["main"],
   plugins: [
+    [
+      "semantic-release-chrome",
+      {
+        asset: "paige_v${nextRelease.version}_${branch.name}.zip",
+      },
+    ],
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/github",
       {
-        assets: ["dist/**/*.{js,css}"],
+        assets: ["paige_v${nextRelease.version}_${branch.name}.zip"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
