f06ea9f507b24032e42354f6822bdc7a07a3e6bd
Hamzat Victor <oluwaborihamzat@gmail.com>
2023-12-11 22:39:34 +0100

    chore: configure semantic release

---
+++ release.config.cjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/**/*.{js,css}"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
